755f453dc8e142d9f0c700df37c74e93b4a6eb6c
Felix <t9pafe00@students.oamk.fi>
2019-12-10 12:54:35 +0200

    Combinatorics added

--- javascript.js
+++ javascript.js
@@ -89,7 +89,7 @@ function conversions()
   }
   if(combinatorics_shown == true)
   {
-    combinatorics();
+    combinatorics_toggle();
   }
   if(truth_shown == true)
   {
@@ -113,7 +113,7 @@ function conversion_tables()
   }
   if(combinatorics_shown == true)
   {
-    combinatorics();
+    combinatorics_toggle();
   }
   if(truth_shown == true)
   {
@@ -123,21 +123,29 @@ function conversion_tables()
   {
     rndm();
   }
+
   var value = 0;
 
   var printed = -1;
   var printed_oct = -1;
   var printed_hex = -1;
 
+  document.getElementById("t0").classList.toggle("conversion_table_hide")
+  document.getElementById("t1").classList.toggle("conversion_table_hide")
+  document.getElementById("t2").classList.toggle("conversion_table_hide")
+  document.getElementById("t3").classList.toggle("conversion_table_hide")
+
   while(value < 51)
   {
     if(printed == -1)
     {
+      printed_dec = value
       printed = value.toString(2);
       printed_oct = value.toString(8);
       printed_hex = value.toString(16);
     }
     else{
+      printed_dec = printed_dec + "<br>" + value
       printed = printed + "<br>" + value.toString(2);
       printed_oct = printed_oct + "<br>" + value.toString(8);
       printed_hex = printed_hex + "<br>" + value.toString(16);
@@ -146,12 +154,93 @@ function conversion_tables()
   }
   if(conv_tables_shown == true)
   {
+    printed_dec = "";
     printed = "";
     printed_oct = "";
     printed_hex = "";
   }
-  document.querySelector(".bin_table").innerHTML = printed;
-  document.querySelector(".oct_table").innerHTML = printed_oct;
-  document.querySelector(".hex_table").innerHTML = printed_hex;
+  document.querySelector(".dec_table").innerHTML = "Dec: <br>" + printed_dec;
+  document.querySelector(".bin_table").innerHTML = "Bin: <br>" + printed;
+  document.querySelector(".oct_table").innerHTML = "Oct: <br>" + printed_oct;
+  document.querySelector(".hex_table").innerHTML = "Hex: <br>" + printed_hex;
   conv_tables_shown = !conv_tables_shown;
+}
+
+function combinatorics()
+{
+  m_element = document.getElementById("m").value;
+  n_element = document.getElementById("n").value;
+  m_fact = factorial(m_element);
+  n_fact = factorial(n_element);
+  m_n = m_element - n_element;
+  m_n_fact = factorial(m_n);
+  replace = false;
+  no_replace = false;
+  if( document.getElementById("r1").checked == true)
+  {
+    replace = true;
+    no_replace = false;
+  }
+  else if( document.getElementById("r2").checked == true)
+  {
+    replace = false;
+    no_replace = true;
+  }
+
+  if( replace == true)
+  {
+    result = Math.pow(m_element, n_element);
+    document.querySelector(".comb_result").innerHTML = result;
+  }
+
+  if( no_replace == true)
+  {
+    result = m_fact / m_n_fact;
+    document.querySelector(".comb_result").innerHTML = result;
+  }
+
+  if( document.getElementById("c1").checked == false )
+  {
+    result = m_fact / (n_fact * m_n_fact)
+    document.querySelector(".comb_result").innerHTML = result;
+  }
+}
+
+function factorial(x)
+{
+  if (x == 0)
+ {
+    return 1;
+ }
+  return x * factorial(x-1);      
+}
+
+function active_replacement()
+{
+  document.getElementById("r1").toggleAttribute("disabled");
+  document.getElementById("r1").checked = true;
+  document.getElementById("r2").toggleAttribute("disabled");
+}
+
+function combinatorics_toggle()
+{
+  if(conversions_shown == true)
+  {
+    conversions();
+  }
+  if(conv_tables_shown == true)
+  {
+    conversion_tables();
+  }
+  if(truth_shown == true)
+  {
+    truth();
+  }
+  if(rndm_shown == true)
+  {
+    rndm();
+  }
+
+  combinatorics_shown = !combinatorics_shown;
+  document.querySelector(".combinatorics").classList.toggle("comb_hide");
 }
